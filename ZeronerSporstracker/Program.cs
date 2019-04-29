b42f68fa03696917a243527d80ea5d014d57cd2a
Martin Slezák <xslezak.martin@centrum.cz>
2019-04-29 21:44:22 +0200

    init commit

---
+++ ZeronerSporstracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZeronerSporstracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
